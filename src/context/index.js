09520039dd05058c47b0dad3afd431a580489687
Piotr Buczek <piotr.buczek37@gmail.com>
2021-01-03 17:56:33 +0100

    Added Context

---
+++ src/context/index.js
@@ -0,0 +1,15 @@
+import {
+    ProjectsContext,
+    ProjectsProvider,
+    useProjectsValue
+} from './project-context';
+
+import {
+    SelectedProjectContext,
+    SelectedProjectProvider,
+    useSelectedProjectValue
+} from './selected-project-content';
+
+export {ProjectsContext, ProjectsProvider, useProjectsValue,SelectedProjectContext,
+    SelectedProjectProvider,
+    useSelectedProjectValue};
